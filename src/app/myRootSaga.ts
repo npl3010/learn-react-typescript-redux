c9fdf2c02917448602999a7762b36ef2c121929d
Linh Nguyen <nguyenphuclinh3010@gmail.com>
2022-04-06 20:48:57 +0700

    chore: Apply redux-saga middleware, create and run Sagas

---
+++ src/app/myRootSaga.ts
@@ -0,0 +1,5 @@
+
+function* myRootSaga() {
+}
+
+export default myRootSaga;
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,23 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import myRootSaga from './myRootSaga';
 import counterReducer from '../features/counter/counterSlice';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: true,
+      immutableCheck: true,
+      serializableCheck: true,
+    }).concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(myRootSaga);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
